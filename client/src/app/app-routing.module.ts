84ffdd7432d5958b4844fb3b788228dfe3986c12
“indavid” <david@gmail.com>
2023-06-05 09:35:54 -0400

    Add Routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { GridComponent } from './grid/grid.component';
+import { GridItemComponent } from './grid-item/grid-item.component';
+import { GridItemDetailComponent } from './grid-item-detail/grid-item-detail.component';
+
+const routes: Routes = [
+  { path: '', component: GridComponent },
+  { path: 'grid-item-detail', component: GridItemDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- client/src/app/grid/grid.component.ts
+++ client/src/app/grid/grid.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-grid',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./grid.component.scss'],
 })
 export class GridComponent {
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('').subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
   items: number[] = Array(10).fill(0);
 }
